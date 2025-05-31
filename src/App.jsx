864cc782bff32df11ddad0472401263162aa6f88
rjun <rjun@archlinux.apollo>
2025-05-31 21:32:28 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import MovieCard from './components/MovieCard'
+
+function App() {
+  return (
+      <>
+        <MovieCard movie = {{title: "Movie32", release_date: "2025"}}/>
+      </>
+  );
+}
+
+export default App
