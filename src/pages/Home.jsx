23c05106cf53c8b10c290333474fdb5ecfbb26af
rjun <rjun@archlinux.apollo>
2025-06-01 01:05:25 +0530

    Working Search Function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,30 @@
+import { useState } from "react";
 import MovieCard from "../components/MovieCard"
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         {id: 1, title: "Interstellar", release_date: "2015"},
         {id: 2, title: "Inception", release_date: "2019"},
         {id: 3 , title: "The Matrix", release_date: "1999"},
     ]
 
-    const handleSearch = () => {
-
+    const handleSearch = (e) => {
+    e.preventDefault()
     }
 
     return ( <div className="home">
         <form onSubmit={handleSearch} className="search-form">
-        <input type = "text" placeholder="Search for Movies..." className="search-input" />
+        <input type = "text" placeholder="Search for Movies..." className="search-input" 
+        value = {searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)} />
         <button type="submit" className="search-button">Search</button>
         </form>
         <div className="movie-grid">
-        {movies.map((movie) => {
-            return <MovieCard movie={movie} key={movie.id} />
-        })}
+        {movies.map((movie) => 
+            movie.title.toLowerCase().startsWith(searchQuery) && ( <MovieCard movie={movie} key={movie.id} /> )
+        )}
         </div>
         </div>
     );
