aa1a539b2f8175acaad5e325883a554390ebe47f
rjun <rjun@archlinux.apollo>
2025-06-01 09:19:07 +0530

    Added basic navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home.jsx'
 import Favorites from './pages/Favorites.jsx';
+import NavBar from './components/NavBar.jsx';
 
 function App() {
   return (
+    <div>
+      <NavBar/>
     <main className="main-content">
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/favorites" element={<Favorites />}/>
       </Routes>
       </main>
+      </div>
   );
 }
 
